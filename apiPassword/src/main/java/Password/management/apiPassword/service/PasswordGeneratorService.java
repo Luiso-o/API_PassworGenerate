87f73bbb23552e4f70dc2db4ef94d553ebed232e
Luiso-o <cheportillo@gmail.com>
2023-12-05 20:10:33 +0100

    capa password

--- apiPassword/src/main/java/Password/management/apiPassword/service/PasswordGeneratorService.java
+++ apiPassword/src/main/java/Password/management/apiPassword/service/PasswordGeneratorService.java
@@ -1,9 +1,11 @@
 package Password.management.apiPassword.service;
 
 import Password.management.apiPassword.Dto.PasswordGeneratorDto;
+import Password.management.apiPassword.document.PasswordDocument;
 
 import java.util.List;
 
 public interface PasswordGeneratorService {
     List<PasswordGeneratorDto> generatePasswords (int length, int quantity);
+    PasswordGeneratorDto savePassword(String password, String name);
 }
